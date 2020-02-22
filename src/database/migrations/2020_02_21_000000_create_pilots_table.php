5ee4352d43729f13feff95b8f709046073c87de1
Xavier Mallafre <xvrmallafre@gmail.com>
2020-02-22 11:02:59 +0100

    added migrations to the module

---
+++ src/database/migrations/2020_02_21_000000_create_pilots_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePilotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pilots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 120);
+            $table->string('gender', 32);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pilots');
+    }
+}
---
+++ src/database/migrations/2020_02_21_000000_create_starships_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStarshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('starships', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 120);
+            $table->string('model', 120);
+            $table->string('manufacturer', 255);
+            $table->string('starship_class', 120);
+            $table->string('cost_in_credits', 120);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('starships');
+    }
+}
